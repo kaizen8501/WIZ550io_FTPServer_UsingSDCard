08a78b854af9ba758128c8363e04cc83190d18c6
kaizen8501 <kaizen8501@gmail.com>
2014-08-27 14:31:06 +0900

    WIZ550io FTP Server
    First Release

---
+++ WIZ550io_FTPServer_UsingSDCard/WIZ550io_FTPServer_UsingSDCard.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _WIZ550io_FTPServer_UsingSDCard_H_
+#define _WIZ550io_FTPServer_UsingSDCard_H_
+#include "Arduino.h"
+//add your includes for the project WIZ550io_FTPServer_UsingSDCard here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project WIZ550io_FTPServer_UsingSDCard here
+
+
+
+
+//Do not add code below this line
+#endif /* _WIZ550io_FTPServer_UsingSDCard_H_ */
